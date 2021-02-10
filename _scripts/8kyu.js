00dfb41936cbc2024a7c096ced1eed9d2ffbe0e3
Marcone M. Mendonça <marconemendonca@ymail.com>
2021-02-10 12:42:00 -0300

    Kata "Welcome!".

--- _scripts/8kyu.js
+++ _scripts/8kyu.js
@@ -190,3 +190,34 @@ const aliasGen = (name, surName) => {
     return `${firstName[nameFirstLetter]} ${surname[surNameFirstLetter]}`;
 
 }
+
+/** Kata "Welcome!" on:
+https://www.codewars.com/kata/577ff15ad648a14b780000e7/train/javascript  */
+
+const languages = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+}
+
+const greet = greetLanguage => {
+    if (languages[greetLanguage] === undefined)
+        return "Welcome";
+
+    return languages[greetLanguage];
+}
+
