cb219057b66db44b60f5130dd2f4a9ce95c5f908
Marcone M. Mendonça <marconemendonca@ymail.com>
2021-02-14 22:59:27 -0300

    Kata "Is a number prime?".

---
+++ _scripts/6kyu.js
@@ -0,0 +1,23 @@
+/*Kata "Is a number prime?" on:
+ https://www.codewars.com/kata/5262119038c0985a5b00029f/train/javascript */
+
+const isPrime = number => {
+
+    if (number < 2) //2 is the first prime numberber:
+        return false;
+
+    const sqrt = Math.round(Math.sqrt(number));
+
+    for (let i = 2; i <= sqrt; i++) {
+
+        /** If IS possible to divide "number" by any integer below its square root,
+         * then "number" isn't a prime number:
+         * */
+        if (number % i === 0)
+            return false;
+
+    }
+
+    return true; // If INS'T possible the previous division, "number" is a prime numbe.
+}
+
