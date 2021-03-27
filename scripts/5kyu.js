ae8618eea4bf5f84cec78baf3034eaa2686c3329
Marcone M. Mendonça <marconemendonca@ymail.com>
2021-03-27 18:32:38 -0300

    Solving "Human Readable Time" - 5kyu.

--- scripts/5kyu.js
+++ scripts/5kyu.js
@@ -2,7 +2,6 @@
  https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/javascript */
 
 const pigIt = str => {
-    debugger
     return str.split(" ").map(word => {
         if (word === "?" || word === "!")
             return word;
@@ -10,5 +9,46 @@ const pigIt = str => {
         return `${word.slice(1)}${word.slice(0, 1)}ay`
     }).join(" ");
 }
+// console.log(pigIt('Pig latin is cool'));
 
-console.log(pigIt('Pig latin is cool'));
+/*Kata "Human Readable Time" on:
+ https://www.codewars.com/kata/52685f7382004e774f0001f7/train/javascript */
+
+const getIntegerPart = value => {
+    value = value.toString();
+    if (value.includes(".")) {
+        const dotIndex = value.indexOf(".");
+        return parseInt(value.slice(0, dotIndex));
+    }
+    return parseInt(value);
+}
+
+const humanReadable = seconds => {
+    if (seconds) {
+
+        let hours = 0;
+        let minutes = getIntegerPart(seconds / 60);
+
+        if (minutes) {
+            hours = getIntegerPart(minutes / 60);
+            minutes -= hours * 60;
+            seconds -= ((hours * 3600) + (minutes * 60)) // 1h === 3600 minutes and 1min === 60 seconds.
+        }
+
+        hours = hours.toString();
+        minutes = minutes.toString();
+        seconds = seconds.toString();
+
+        if (hours.length === 1)
+            hours = `0${hours}`;
+
+        if (minutes.length === 1)
+            minutes = `0${minutes}`;
+
+        if (seconds.length === 1)
+            seconds = `0${seconds}`;
+
+        return `${hours}:${minutes}:${seconds}`;
+    }
+    return "00:00:00";
+}
