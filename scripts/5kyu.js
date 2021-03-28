1f7b3dc292de1ee29cc083659d6d542c4689a171
Marcone M. Mendonça <marconemendonca@ymail.com>
2021-03-27 22:47:56 -0300

    Solving "RGB To Hex Conversion" - 5kyu.

--- scripts/5kyu.js
+++ scripts/5kyu.js
@@ -91,3 +91,43 @@ const validParentheses = parentheses => {
 // console.log(validParentheses("())"));
 // console.log(validParentheses(")()()()("));
 // console.log(validParentheses("()()()()()()()()()()(())()()()((()"));
+
+/*Kata "RGB To Hex Conversion" on:
+ https://www.codewars.com/kata/513e08acc600c94f01000001 */
+
+const getHexadecimal = value => {
+    if (value < 1)
+        return "00";
+
+    if (value > 255)
+        value = 255;
+
+    if (value === 255)
+        return "FF";
+
+    let integerPart;
+    let module;
+    const hexaBase = { 0: 0, 1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6, 7: 7, 8: 8, 9: 9, 10: "A", 11: "B", 12: "C", 13: "D", 14: "E", 15: "F" };
+    const resultList = []
+
+    do {
+        integerPart = Math.floor(value / 16);
+        module = value % 16;
+        resultList.push(module);
+        value = integerPart;
+
+        if (integerPart <= 1)
+            resultList.push(integerPart);
+
+    } while (integerPart > 1)
+
+    return `${hexaBase[resultList[1]]}${hexaBase[resultList[0]]}`;
+}
+
+const rgb = (r, g, b) => getHexadecimal(r) + getHexadecimal(g) + getHexadecimal(b);
+
+// console.log(rgb(0, 0, 0));
+// console.log(rgb(0, 0, -20));
+// console.log(rgb(300, 255, 255));
+// console.log(rgb(173, 255, 47));
+// console.log(rgb(8, 11, 248));
